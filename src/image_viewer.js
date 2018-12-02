ddc6aaac6e5177f0207245daa4f15be448ba5553
HariS <HariS@Hari.gateway>
2018-12-02 20:46:09 +1100

    webpack basic code split with two bundle files

---
+++ src/image_viewer.js
@@ -0,0 +1,11 @@
+// import big from '../assets/big.jpg';
+import small from '../assets/small.jpg';
+
+import '../styles/image_viewer.css';
+
+export default () => { 
+  const smallImage = document.createElement('img');
+  smallImage.src = small;
+
+  document.body.appendChild(smallImage);
+};
---
+++ src/index.js
@@ -0,0 +1,17 @@
+// import sum from './sum';
+// import './image_viewer';
+
+// const total = sum(10,5);
+
+// console.log(total);
+
+const button = document.createElement('button');
+button.innerText = 'Click me';
+button.onclick = () => {
+  System.import('./image_viewer').then(module => {
+    console.log(module);
+    module.default();
+  })
+}
+
+document.body.appendChild(button);
